9770507b08412e66d23b546910aef91ac8c9424d
sirishak9 <sirishakotha9@gmail.com>
2020-11-22 17:53:54 -0800

    commit#2 for @v2.0 release
    commit #2 for @v2.0 release

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,10 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!  changes to source code @v2.0 release" );
     }
 }
