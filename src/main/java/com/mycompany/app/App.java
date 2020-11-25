ab04a399a82e1682453005db29fdde7e9fc01546
sirishak9 <sirishakotha9@gmail.com>
2020-11-24 18:38:31 -0800

    updated changes to V2.0 release
    updated changes to V2.0 release

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!  changes to source code @v2.0 release" );
+        System.out.println( "Hello World!  changes to source code @v2.0 release by Siris" );
     }
 }
